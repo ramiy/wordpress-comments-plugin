32d49d4188c1f473bc873bfc886fde323de3223e
Idan Mitrofanov (@idanm_) <idan1986@gmail.com>
2016-04-24 15:03:36 +0300

    cleanups of syntax

--- inc/class-spotim-export-conversation.php
+++ inc/class-spotim-export-conversation.php
@@ -1,177 +1,181 @@
 <?php
 
 class SpotIM_Export_Conversation {
-	public $post_id;
-	private $comments;
-	private $object;
+    public $post_id;
+    private $comments;
+    private $object;
 
-	public function __construct( $post_id ) {
-		$this->post_id = $post_id;
+    public function __construct( $post_id ) {
+        $this->post_id = $post_id;
 
-		// cache comments
-		$comments = get_comments( apply_filters( 'spotim_comment_query_args', array(
-			'post_id' => $post_id,
-			'type' => 'comment', // include only comments, no trackbacks/pingbacks
-		) ) );
+        // cache comments
+        $comments = get_comments( apply_filters( 'spotim_comment_query_args', array(
+            'post_id' => $post_id,
+            'type' => 'comment', // include only comments, no trackbacks/pingbacks
+        )));
 
-		foreach ( $comments as $comment ) {
-			$this->comments[ $comment->comment_ID ] = $comment;
-		}
+        foreach ( $comments as $comment ) {
+            $this->comments[ $comment->comment_ID ] = $comment;
+        }
 
-		// solidify 'global' object to be used throught this class
-		$this->object = new stdClass;
+        // solidify 'global' object to be used throught this class
+        $this->object = new stdClass;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Check if conversation is empty
-	 *
-	 * @return bool
-	 */
-	public function is_empty() {
-		return empty( $this->comments );
-	}
+    /**
+     * Check if conversation is empty
+     *
+     * @return bool
+     */
+    public function is_empty() {
+        return empty( $this->comments );
+    }
 
-	public function filter_children( $parent_id ) {
-		$collection = array();
+    public function filter_children( $parent_id ) {
+        $collection = array();
 
-		foreach ($this->comments as $comment) {
-			if ( $comment->comment_parent == $parent_id )
-				$collection[] = $comment;
-		}
+        foreach ( $this->comments as $comment ) {
+            if ( $comment->comment_parent == $parent_id ) {
+                $collection[] = $comment;
+            }
+        }
 
-		return $collection;
-	}
+        return $collection;
+    }
 
-	public static function filter_parents($comment) {
-		return $comment->comment_parent == 0;
-	}
+    public static function filter_parents( $comment ) {
+        return $comment->comment_parent == 0;
+    }
 
-	public function export() {
-		// Do we even have any comments to begin with?
-		if ( empty($this->comments) )
-			return false;
+    public function export() {
 
-		$o = &$this->object; // pointer
+        // Do we even have any comments to begin with?
+        if ( empty( $this->comments ) ) {
+            return false;
+        }
 
-		// set URL of post
-		$o->site_url = get_permalink( $this->post_id );
-		
-		// then get all comment IDs
-		$o->comments_ids = array_values( wp_list_pluck( $this->get_top_level_comments(), 'comment_ID' ) );
+        // set URL of post
+        $this->object->site_url = get_permalink( $this->post_id );
 
-		// first, we need the tree
-		$o->tree = $this->get_tree();
+        // then get all comment IDs
+        $this->object->comments_ids = array_values(
+            wp_list_pluck( $this->get_top_level_comments(), 'comment_ID' )
+        );
 
-		// aggregate all comments (messages)
-		$o->messages = $this->aggregate_messages();
+        // first, we need the tree
+        $this->object->tree = $this->get_tree();
 
-		// last but not least, users.
-		$o->users = $this->aggregate_users();
+        // aggregate all comments (messages)
+        $this->object->messages = $this->aggregate_messages();
 
-		return apply_filters( 'spotim_conversation_ready', $o );
-	}
+        // last but not least, users.
+        $this->object->users = $this->aggregate_users();
 
-	public function aggregate_users() {
-		$users = array();
+        return apply_filters( 'spotim_conversation_ready', $this->object );
+    }
 
-		foreach ( $this->comments as $comment ) {
-			if ( ! trim($comment->comment_author_email) ) {
-				// this comment has no email behind it. pick a random user ID to associate with.
-				$_orderby_fields = array(
-					'ID',
-					'display_name',
-					'name',
-					'email',
-					'registered',
-				);
+    public function aggregate_users() {
+        $users = array();
 
-				$_order_options = array(
-					'ASC',
-					'DESC',
-				);
+        foreach ( $this->comments as $comment ) {
+            if ( ! trim( $comment->comment_author_email ) ) {
 
-				$users_query = get_users( array(
-					'number' => 1,
-					'orderby' => $_orderby_fields[ rand( 0, count($_orderby_fields) - 1 ) ],
-					'order' => $_order_options[ rand( 0, count($_order_options) - 1 ) ],
-				) );
+                // this comment has no email behind it. pick a random user ID to associate with.
+                $_orderby_fields = array(
+                    'ID',
+                    'display_name',
+                    'name',
+                    'email',
+                    'registered'
+                );
 
-				if ( empty($users_query) )
-					continue;
+                $_order_options = array( 'ASC', 'DESC' );
 
-				$user = current( $users_query );
+                $users_query = get_users( array(
+                    'number' => 1,
+                    'orderby' => $_orderby_fields[ rand( 0, count( $_orderby_fields ) - 1 ) ],
+                    'order' => $_order_options[ rand( 0, count( $_order_options ) - 1 ) ],
+                ) );
 
-				$users[ $user->ID ] = array(
-					'user_name' => apply_filters( 'get_comment_author', $comment->comment_author, $comment->comment_ID, $comment ),
-					'display_name' => apply_filters( 'get_comment_author', $comment->comment_author, $comment->comment_ID, $comment ),
-					'email' => $comment->comment_author_email,
-				);
-			} else {
-				$users[ $comment->comment_author_email ] = array(
-					'user_name' => apply_filters( 'get_comment_author', $comment->comment_author, $comment->comment_ID, $comment ),
-					'display_name' => apply_filters( 'get_comment_author', $comment->comment_author, $comment->comment_ID, $comment ),
-					'email' => $comment->comment_author_email,
-				);
-			}
-		}
+                if ( empty( $users_query ) ) {
+                    continue;
+                }
 
-		return $users;
-	}
+                $user = current( $users_query );
 
-	public function aggregate_messages() {
-		// if tree is not rendered yet, do it now.
-		if ( empty( $this->object->tree ) ) {
-			$this->object->tree = $this->get_tree();
-		}
+                $users[ $user->ID ] = array(
+                    'user_name' => apply_filters( 'get_comment_author', $comment->comment_author, $comment->comment_ID, $comment ),
+                    'display_name' => apply_filters( 'get_comment_author', $comment->comment_author, $comment->comment_ID, $comment ),
+                    'email' => $comment->comment_author_email,
+                );
+            } else {
+                $users[ $comment->comment_author_email ] = array(
+                    'user_name' => apply_filters( 'get_comment_author', $comment->comment_author, $comment->comment_ID, $comment ),
+                    'display_name' => apply_filters( 'get_comment_author', $comment->comment_author, $comment->comment_ID, $comment ),
+                    'email' => $comment->comment_author_email,
+                );
+            }
+        }
 
-		$comments = array();
+        return $users;
+    }
 
-		foreach ( $this->comments as $comment_id => $comment ) {
-			$comment_is_anonymous = ( '' === trim($comment->comment_author_email) );
+    public function aggregate_messages() {
+        // if tree is not rendered yet, do it now.
+        if ( empty( $this->object->tree ) ) {
+            $this->object->tree = $this->get_tree();
+        }
 
-			$comments[ $comment->comment_ID ] = array(
-				'content' => apply_filters( 'get_comment_text', $comment->comment_content, $comment, array() ),
-				'written_at' => strtotime( $comment->comment_date_gmt ),
-			);
+        $comments = array();
 
-			// if comment isn't anonymous, append user ID
-			if ( ! $comment_is_anonymous )
-				$comments[ $comment->comment_ID ]['user_id'] = $comment->comment_author_email;
-			else
-				$comments[ $comment->comment_ID ]['anonymous'] = true;
-		}
-		
-		return $comments;
-	}
+        foreach ( $this->comments as $comment_id => $comment ) {
+            $comment_is_anonymous = ( '' === trim( $comment->comment_author_email ) );
 
-	public function get_tree() {
-		$parent_comments = $this->get_top_level_comments();
-		$bank = array();
+            $comments[ $comment->comment_ID ] = array(
+                'content' => apply_filters( 'get_comment_text', $comment->comment_content, $comment, array() ),
+                'written_at' => strtotime( $comment->comment_date_gmt ),
+            );
 
-		// take care of figuring out tree
-		foreach ($parent_comments as $comment) {
-			$this->traverse($comment->comment_ID, $bank);
-		}
-		
-		return (object) $bank;
-	}
+            // if comment isn't anonymous, append user ID
+            if ( ! $comment_is_anonymous ) {
+                $comments[ $comment->comment_ID ]['user_id'] = $comment->comment_author_email;
+            } else {
+                $comments[ $comment->comment_ID ]['anonymous'] = true;
+            }
+        }
 
-	public function get_top_level_comments() {
-		return array_filter($this->comments, array($this, 'filter_parents'));
-	}
+        return $comments;
+    }
 
-	private function traverse($comment_id, &$bank) {
-		$child_comments = $this->filter_children($comment_id);
+    public function get_tree() {
+        $parent_comments = $this->get_top_level_comments();
+        $bank = array();
 
-		// if no comments under this one, we're ending it here
-		if ( ! empty($child_comments) ) {
-			$bank[$comment_id] = wp_list_pluck( $child_comments, 'comment_ID' );
+        // take care of figuring out tree
+        foreach ( $parent_comments as $comment ) {
+            $this->traverse( $comment->comment_ID, $bank );
+        }
 
-			// recurse down the tree
-			foreach ( $child_comments as $comment )
-				$this->traverse($comment->comment_ID, $bank);
-		}
-	}
+        return (object) $bank;
+    }
+
+    public function get_top_level_comments() {
+        return array_filter( $this->comments, array( $this, 'filter_parents' ) );
+    }
+
+    private function traverse( $comment_id, &$bank ) {
+        $child_comments = $this->filter_children( $comment_id );
+
+        // if no comments under this one, we're ending it here
+        if ( ! empty( $child_comments ) ) {
+            $bank[ $comment_id ] = wp_list_pluck( $child_comments, 'comment_ID' );
+
+            // recurse down the tree
+            foreach ( $child_comments as $comment ) {
+                $this->traverse( $comment->comment_ID, $bank );
+            }
+        }
+    }
 }
--- inc/class-spotim-generate-json-conversation.php
+++ inc/class-spotim-generate-json-conversation.php
@@ -1,28 +1,27 @@
 <?php
 
 class SpotIM_JSON_Conversation {
-
     public $post_id;
     private $comments;
     private $object;
 
-    public function __construct($post_id) {
-	$this->post_id = $post_id;
+    public function __construct( $post_id ) {
+        $this->post_id = $post_id;
 
-	// cache comments
-	$comments = get_comments(apply_filters('spotim_comment_query_args', array(
-	    'post_id' => $post_id,
-	    'type' => 'comment', // include only comments, no trackbacks/pingbacks
-	)));
+        // cache comments
+        $comments = get_comments( apply_filters( 'spotim_comment_query_args', array(
+            'post_id' => $post_id,
+            'type' => 'comment', // include only comments, no trackbacks/pingbacks
+        )));
 
-	foreach ($comments as $comment) {
-	    $this->comments[$comment->comment_ID] = $comment;
-	}
+        foreach ( $comments as $comment ) {
+            $this->comments[ $comment->comment_ID ] = $comment;
+        }
 
-	// solidify 'global' object to be used throught this class
-	$this->object = new stdClass;
+        // solidify 'global' object to be used throught this class
+        $this->object = new stdClass;
 
-	return $this;
+        return $this;
     }
 
     /**
@@ -31,162 +30,164 @@ class SpotIM_JSON_Conversation {
      * @return bool
      */
     public function is_empty() {
-	return empty($this->comments);
+        return empty( $this->comments );
     }
 
-    public function filter_children($parent_id) {
-	$collection = array();
+    public function filter_children( $parent_id ) {
+        $collection = array();
 
-	foreach ($this->comments as $comment) {
-	    if ($comment->comment_parent == $parent_id)
-		$collection[] = $comment;
-	}
+        foreach ( $this->comments as $comment ) {
+            if ( $comment->comment_parent == $parent_id ) {
+                $collection[] = $comment;
+            }
+        }
 
-	return $collection;
+        return $collection;
     }
 
-    public static function filter_parents($comment) {
-	return $comment->comment_parent == 0;
+    public static function filter_parents( $comment ) {
+        return $comment->comment_parent == 0;
     }
 
     public function export() {
-	// Do we even have any comments to begin with?
-	if (empty($this->comments))
-	    return false;
 
-	$o = &$this->object; // pointer
-	// set URL of post
-	$o->post_id = $this->post_id;
-        
-        $o->published_at = get_the_time('U', $this->post_id);;
+        // Do we even have any comments to begin with?
+        if ( empty( $this->comments ) ) {
+            return false;
+        }
 
-	// set URL of post
-	$o->conversation_url = site_url('?p=' . $this->post_id);
+        // set URL of post
+        $this->object->post_id = $this->post_id;
 
-	// then get all comment IDs
-	$o->comments_ids = array_values(wp_list_pluck($this->get_top_level_comments(), 'comment_ID'));
+        $this->object->published_at = get_the_time( 'U', $this->post_id );
 
-	// first, we need the tree
-	$o->tree = $this->get_tree();
+        // set URL of post
+        $this->object->conversation_url = site_url( '?p=' . $this->post_id );
 
-	// aggregate all comments (messages)
-	$o->messages = $this->aggregate_messages();
+        // then get all comment IDs
+        $this->object->comments_ids = array_values( wp_list_pluck( $this->get_top_level_comments(), 'comment_ID') );
 
-	// last but not least, users.
-	$o->users = $this->aggregate_users();
+        // first, we need the tree
+        $this->object->tree = $this->get_tree();
 
-	return apply_filters('spotim_conversation_ready', $o);
+        // aggregate all comments (messages)
+        $this->object->messages = $this->aggregate_messages();
+
+        // last but not least, users.
+        $this->object->users = $this->aggregate_users();
+
+        return apply_filters( 'spotim_conversation_ready', $this->object );
     }
 
     public function aggregate_users() {
-	$users = array();
+        $users = array();
 
-	foreach ($this->comments as $comment) {
-	    if (trim($comment->user_id)) {
-		// this comment has no email behind it. pick a random user ID to associate with.
-		$_orderby_fields = array(
-		    'ID',
-		    'display_name',
-		    'name',
-		    'email',
-		    'registered',
-		);
+        foreach ( $this->comments as $comment ) {
+            if ( trim( $comment->user_id ) ) {
 
-		$_order_options = array(
-		    'ASC',
-		    'DESC',
-		);
+                // this comment has no email behind it. pick a random user ID to associate with.
+                $_orderby_fields = array(
+                    'ID',
+                    'display_name',
+                    'name',
+                    'email',
+                    'registered'
+                );
 
-		$users_query = get_users(array(
-		    'number' => 1,
-		    'orderby' => $_orderby_fields[rand(0, count($_orderby_fields) - 1)],
-		    'order' => $_order_options[rand(0, count($_order_options) - 1)],
-		));
+                $_order_options = array( 'ASC', 'DESC' );
 
-		if (empty($users_query))
-		    continue;
+                $users_query = get_users( array(
+                    'number' => 1,
+                    'orderby' => $_orderby_fields[ rand( 0, count( $_orderby_fields ) - 1) ],
+                    'order' => $_order_options[ rand( 0, count( $_order_options ) - 1 ) ],
+                ));
 
-		$user = current($users_query);
+                if ( empty( $users_query ) ) {
+                    continue;
+                }
 
-		$users[$user->ID] = array(
-		    'user_name' => apply_filters('get_comment_author', $comment->comment_author, $comment->comment_ID, $comment),
-		    'display_name' => apply_filters('get_comment_author', $comment->comment_author, $comment->comment_ID, $comment),
-		    'email' => $comment->comment_author_email,
-		    //'website_url' => $user->user_url
-		);
-	    } else {
-		$users[$comment->comment_author_email] = array(
-		    'user_name' => apply_filters('get_comment_author', $comment->comment_author, $comment->comment_ID, $comment),
-		    'display_name' => apply_filters('get_comment_author', $comment->comment_author, $comment->comment_ID, $comment),
-		    'email' => $comment->comment_author_email,
-		    //'website_url' => $user->user_url
-		);
-	    }
-	    if($user->user_url != ''){
-		$users[$user->ID]['website_url'] = $user->user_url;
-	    }else{
-		$users[$user->ID]['website_url'] = '';
-	    }
-	}
+                $user = current( $users_query );
 
-	return $users;
+                $users[ $user->ID ] = array(
+                    'user_name' => apply_filters( 'get_comment_author', $comment->comment_author, $comment->comment_ID, $comment ),
+                    'display_name' => apply_filters( 'get_comment_author', $comment->comment_author, $comment->comment_ID, $comment ),
+                    'email' => $comment->comment_author_email,
+                    //'website_url' => $user->user_url
+                );
+            } else {
+                $users[ $comment->comment_author_email ] = array(
+                    'user_name' => apply_filters( 'get_comment_author', $comment->comment_author, $comment->comment_ID, $comment ),
+                    'display_name' => apply_filters( 'get_comment_author', $comment->comment_author, $comment->comment_ID, $comment ),
+                    'email' => $comment->comment_author_email,
+                    //'website_url' => $user->user_url
+                );
+            }
+
+            if( ! empty( $user->user_url ) ) {
+                $users[$user->ID]['website_url'] = $user->user_url;
+            } else {
+                $users[ $user->ID ]['website_url'] = '';
+            }
+        }
+
+        return $users;
     }
 
     public function aggregate_messages() {
-	// if tree is not rendered yet, do it now.
-	if (empty($this->object->tree)) {
-	    $this->object->tree = $this->get_tree();
-	}
+        // if tree is not rendered yet, do it now.
+        if ( empty( $this->object->tree ) ) {
+            $this->object->tree = $this->get_tree();
+        }
 
-	$comments = array();
+        $comments = array();
 
-	foreach ($this->comments as $comment_id => $comment) {
-	    $comment_is_anonymous = ( '' === trim($comment->comment_author_email) );
+        foreach ( $this->comments as $comment_id => $comment ) {
+            $comment_is_anonymous = ( '' === trim( $comment->comment_author_email ) );
 
-	    $comments[$comment->comment_ID] = array(
-		'content' => apply_filters('get_comment_text', $comment->comment_content, $comment, array()),
-		'written_at' => strtotime($comment->comment_date_gmt),
-	    );
+            $comments[ $comment->comment_ID ] = array(
+                'content' => apply_filters( 'get_comment_text', $comment->comment_content, $comment, array()),
+                'written_at' => strtotime( $comment->comment_date_gmt ),
+            );
 
-	    // if comment isn't anonymous, append user ID
-	    if (!$comment_is_anonymous) {
-		$comments[$comment->comment_ID]['user_id'] = $comment->user_id;
-		$comments[$comment->comment_ID]['anonymous'] = false;
-	    } else {
-		$comments[$comment->comment_ID]['anonymous'] = true;
-	    }
-	}
+            // if comment isn't anonymous, append user ID
+            if ( ! $comment_is_anonymous ) {
+                $comments[ $comment->comment_ID ]['user_id'] = $comment->user_id;
+                $comments[ $comment->comment_ID ]['anonymous'] = false;
+            } else {
+                $comments[ $comment->comment_ID ]['anonymous'] = true;
+            }
+        }
 
-	return $comments;
+        return $comments;
     }
 
     public function get_tree() {
-	$parent_comments = $this->get_top_level_comments();
-	$bank = array();
+        $parent_comments = $this->get_top_level_comments();
+        $bank = array();
 
-	// take care of figuring out tree
-	foreach ($parent_comments as $comment) {
-	    $this->traverse($comment->comment_ID, $bank);
-	}
+        // take care of figuring out tree
+        foreach ( $parent_comments as $comment ) {
+            $this->traverse( $comment->comment_ID, $bank );
+        }
 
-	return (object) $bank;
+        return (object) $bank;
     }
 
     public function get_top_level_comments() {
-	return array_filter($this->comments, array($this, 'filter_parents'));
+        return array_filter( $this->comments, array( $this, 'filter_parents' ) );
     }
 
-    private function traverse($comment_id, &$bank) {
-	$child_comments = $this->filter_children($comment_id);
+    private function traverse( $comment_id, &$bank ) {
+        $child_comments = $this->filter_children( $comment_id );
 
-	// if no comments under this one, we're ending it here
-	if (!empty($child_comments)) {
-	    $bank[$comment_id] = wp_list_pluck($child_comments, 'comment_ID');
+        // if no comments under this one, we're ending it here
+        if ( ! empty( $child_comments ) ) {
+            $bank[ $comment_id ] = wp_list_pluck( $child_comments, 'comment_ID' );
 
-	    // recurse down the tree
-	    foreach ($child_comments as $comment)
-		$this->traverse($comment->comment_ID, $bank);
-	}
+            // recurse down the tree
+            foreach ($child_comments as $comment) {
+                $this->traverse( $comment->comment_ID, $bank );
+            }
+        }
     }
-
 }
