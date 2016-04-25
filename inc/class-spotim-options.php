df112a4e3818843e81e25b4c61544389c2c7ea52
Idan Mitrofanov (@idanm_) <idan1986@gmail.com>
2016-04-25 18:45:30 +0300

    - added lang slug, option_group public variables - added create_options function - rewrite get_options function to get_meta_data - fix get function

--- inc/class-spotim-options.php
+++ inc/class-spotim-options.php
@@ -3,12 +3,14 @@
 class SpotIM_Options {
     private static $instance;
     private $data;
-    public $templates_path, $slug;
+    public $templates_path, $slug, $lang_slug, $option_group;
 
     protected function __construct() {
         $this->templates_path = plugin_dir_path( dirname( __FILE__ ) ) . 'templates/';
         $this->slug = 'wp-spotim-settings';
-        $this->data = $this->get_options();
+        $this->option_group = 'wp-spotim-options';
+        $this->lang_slug = 'wp-spotim';
+        $this->data = $this->get_meta_data();
     }
 
     public static function get_instance() {
@@ -19,19 +21,30 @@ class SpotIM_Options {
         return static::$instance;
     }
 
-    private function get_options() {
-        if ( isset( $this->data ) && is_array( $this->data ) &&
-            ! empty( $this->data ) ) {
-            return $this->data;
+    private function create_options() {
+        $options = array(
+            'enable_comments_replacement' => 1,
+            'enable_comments_on_page' => 0,
+            'spot_id' => ''
+        );
+
+        update_option( $this->slug, $options );
+
+        return $options;
+    }
+
+    public function get_meta_data() {
+        $data = get_option( $this->slug, array() );
+
+        if ( empty( $data ) ) {
+            $data = $this->create_options();
         }
 
-        return apply_filters( 'spotim_options', get_option( $this->slug, array() ) );
+        return $data;
     }
 
     public function get( $key = '', $default_value = false ) {
-        $options = $this->get_options();
-
-        return ! empty( $options[ $key ] ) ? $options[ $key ] : $default_value;
+        return ! empty( $this->data[ $key ] ) ? $this->data[ $key ] : $default_value;
     }
 
     // public function update( $key, $value ) {}
