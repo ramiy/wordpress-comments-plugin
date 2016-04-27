4c7450e4c3884d8f71fa68cd643ad257788fdbd4
Idan Mitrofanov (@idanm_) <idan1986@gmail.com>
2016-04-27 17:29:46 +0300

    remove unused attibute

--- templates/comments-template.php
+++ templates/comments-template.php
@@ -1,6 +1,5 @@
 <div class="spot-im-frame-inpage"
     data-post-id="<?php the_ID(); ?>"
     data-disqus-url="<?php the_permalink(); ?>"
-    data-disqus-identifier="<?php the_ID(); ?> <?php echo site_url(); ?>/?p=<?php the_ID(); ?>"
-    data-wp-import-endpoint="<?php echo site_url('/?p=' . get_the_ID() . '&json-comments'); ?>">
+    data-disqus-identifier="<?php the_ID(); ?> <?php echo site_url(); ?>/?p=<?php the_ID(); ?>">
 </div>
