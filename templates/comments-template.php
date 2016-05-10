f4aa41039d8b48369cbf00c21223a389ce7b4c73
Idan Mitrofanov (@idanm_) <idan1986@gmail.com>
2016-05-10 18:12:16 +0300

    fix for minimized html

--- templates/comments-template.php
+++ templates/comments-template.php
@@ -1,5 +1,4 @@
-<div class="spot-im-frame-inpage"
-    data-post-id="<?php the_ID(); ?>"
-    data-disqus-url="<?php the_permalink(); ?>"
-    data-disqus-identifier="<?php the_ID(); ?> <?php echo site_url(); ?>/?p=<?php the_ID(); ?>">
+<div class="spot-im-comments comments-area">
+    <div class="spot-im-frame-inpage" data-post-id="<?php the_ID(); ?>" data-disqus-url="<?php the_permalink(); ?>" data-disqus-identifier="<?php the_ID(); ?> <?php echo site_url(); ?>/?p=<?php the_ID(); ?>">
+    </div>
 </div>
