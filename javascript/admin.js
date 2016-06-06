c5680d7fb914b5b2f70da5f266038fc4e3676a67
Idan Mitrofanov (@idanm_) <idan1986@gmail.com>
2016-06-07 02:03:18 +0300

    added the ability of handling response’s success and error messages in javascript

--- javascript/admin.js
+++ javascript/admin.js
@@ -1,13 +1,28 @@
 jQuery( document ).ready(function ( $ ) {
 
     $('#import_button').on('click', function( event ) {
-        $this = $(this);
+        var $this = $(this),
+            $messageField = $this.parent().find('.description');
+
+        // empty message field from any text and reset css
+        $messageField
+            .css({ 'color': '' })
+            .html('');
+
+        // disable the import button
         $this.attr( 'disabled', true );
 
         $.post( ajaxurl, { 'action': 'start_import' }, function( response ) {
+            if ( false === response.success ) {
+                $messageField.css({ 'color': '#db3737' });
+            }
+
+            // show response message inside message field
+            $messageField.html( response.data );
+
+            // enable the import button
             $this.attr( 'disabled', false );
 
-            console.log( response );
         }, 'json' );
 
         event.preventDefault();
