4a3ee744bb02f37ed40d48e48b66cfa3641f4868
Rami Yushuvaev <r_a_m_i@hotmail.com>
2017-01-06 03:41:04 +0200

    Remove depricated admin icon fron the plugin settings page

--- templates/admin-template.php
+++ templates/admin-template.php
@@ -1,5 +1,4 @@
 <div class="wrap">
-    <div id="icon-themes" class="icon32"></div>
     <h2 class="spotim-page-title">
         <?php esc_html_e( 'Spot.IM Settings', 'spotim-comments' ); ?>
     </h2>
