7e2c3993946b18c071d10d305a81add5078f7374
Rami Yushuvaev <r_a_m_i@hotmail.com>
2017-01-15 13:24:06 +0200

    Split the fields into 3 types of fields (General, Display, Import) and add the "Auto Import" field

--- inc/class-spotim-settings-fields.php
+++ inc/class-spotim-settings-fields.php
@@ -57,7 +57,20 @@ class SpotIM_Settings_Fields {
      * @return void
      */
     public function general_settings_section_header() {
-        echo '<p>' . esc_html__( 'These are some basic settings for Spot.IM.', 'spotim-comments' ) . '</p>';
+        echo '<p>' . esc_html__( 'Basic settings to integrate your Spot.IM account.', 'spotim-comments' ) . '</p>';
+    }
+
+    /**
+     * Display Settings Section Header
+     *
+     * @since 2.0.0
+     *
+     * @access public
+     *
+     * @return void
+     */
+    public function display_settings_section_header() {
+        echo '<p>' . esc_html__( 'These are some display settings on your theme.', 'spotim-comments' ) . '</p>';
     }
 
     /**
@@ -85,48 +98,22 @@ class SpotIM_Settings_Fields {
     public function register_general_section() {
         add_settings_section(
             'general_settings_section',
-            esc_html__( 'Commenting Options', 'spotim-comments' ),
+            esc_html__( 'General Options', 'spotim-comments' ),
             array( $this, 'general_settings_section_header' ),
             $this->options->slug
         );
 
-        add_settings_field(
-            'enable_comments_replacement',
-            esc_html__( 'Enable Spot.IM comments', 'spotim-comments' ),
-            array( 'SpotIM_Form_Helper', 'yes_no_fields' ),
-            $this->options->slug,
-            'general_settings_section',
-            array(
-                'id' => 'enable_comments_replacement',
-                'page' => $this->options->slug,
-                'value' => $this->options->get( 'enable_comments_replacement' )
-            )
-        );
-
-        add_settings_field(
-            'enable_comments_on_page',
-            esc_html__( 'Enable Spot.IM on pages', 'spotim-comments' ),
-            array( 'SpotIM_Form_Helper', 'yes_no_fields' ),
-            $this->options->slug,
-            'general_settings_section',
-            array(
-                'id' => 'enable_comments_on_page',
-                'page' => $this->options->slug,
-                'value' => $this->options->get( 'enable_comments_on_page' )
-            )
-        );
-
         $translated_spot_id_description = sprintf(
-		    __('Find your Spot ID at the Spot.IM\'s <a href="%s" target="_blank">Admin Dashboard</a> under Integrations section.' , 'spotim-comments'),
+		    __('Find your Spot ID at the <a href="%s" target="_blank">Spot.IM\'s Admin Dashboard</a> under "Features" section.' , 'spotim-comments'),
             'https://admin.spot.im/login'
-        ) . '<br />' . sprintf(
+        ) . ' ' . sprintf(
             __('Don\'t have an account? <a href="%s" target="_blank">Create</a> one for free!' , 'spotim-comments'),
             'https://admin.spot.im/login'
         );
 
         add_settings_field(
             'spot_id',
-            esc_html__( 'Your Spot ID', 'spotim-comments' ),
+            esc_html__( 'Spot ID', 'spotim-comments' ),
             array( 'SpotIM_Form_Helper', 'text_field' ),
             $this->options->slug,
             'general_settings_section',
@@ -137,6 +124,65 @@ class SpotIM_Settings_Fields {
                 'value' => $this->options->get( 'spot_id' )
             )
         );
+
+    }
+
+    /**
+     * Register Display Section
+     *
+     * @since 2.0.0
+     *
+     * @access public
+     *
+     * @return void
+     */
+    public function register_display_section() {
+        add_settings_section(
+            'display_settings_section',
+            esc_html__( 'Display Options', 'spotim-comments' ),
+            array( $this, 'display_settings_section_header' ),
+            $this->options->slug
+        );
+
+        add_settings_field(
+            'enable_comments_replacement',
+            esc_html__( 'Enable Spot.IM comments', 'spotim-comments' ),
+            array( 'SpotIM_Form_Helper', 'yes_no_fields' ),
+            $this->options->slug,
+            'display_settings_section',
+            array(
+                'id' => 'enable_comments_replacement',
+                'page' => $this->options->slug,
+                'value' => $this->options->get( 'enable_comments_replacement' )
+            )
+        );
+
+        add_settings_field(
+            'enable_questions_replacement',
+            esc_html__( 'Enable Spot.IM questions', 'spotim-comments' ),
+            array( 'SpotIM_Form_Helper', 'yes_no_fields' ),
+            $this->options->slug,
+            'display_settings_section',
+            array(
+                'id' => 'enable_questions_replacement',
+                'page' => $this->options->slug,
+                'value' => $this->options->get( 'enable_questions_replacement' )
+            )
+        );
+
+        add_settings_field(
+            'enable_comments_on_page',
+            esc_html__( 'Enable Spot.IM on pages', 'spotim-comments' ),
+            array( 'SpotIM_Form_Helper', 'yes_no_fields' ),
+            $this->options->slug,
+            'display_settings_section',
+            array(
+                'id' => 'enable_comments_on_page',
+                'page' => $this->options->slug,
+                'value' => $this->options->get( 'enable_comments_on_page' )
+            )
+        );
+
     }
 
     /**
@@ -184,16 +230,65 @@ class SpotIM_Settings_Fields {
             )
         );
 
+		/*
+        add_settings_field(
+            'enable_auto_import',
+            esc_html__( 'Enable Auto Import', 'spotim-comments' ),
+            array( 'SpotIM_Form_Helper', 'yes_no_fields' ),
+            $this->options->slug,
+            'import_settings_section',
+            array(
+                'id' => 'enable_autom_import',
+                'page' => $this->options->slug,
+                'value' => $this->options->get( 'enable_autom_import' )
+            )
+        );
+
+        add_settings_field(
+            'import_recurrence',
+            esc_html__( 'Import Recurrence', 'spotim-comments' ),
+            array( 'SpotIM_Form_Helper', 'text_field' ),
+            $this->options->slug,
+            'import_settings_section',
+            array(
+                'id' => 'import_recurrence',
+                'page' => $this->options->slug,
+                'description' => esc_html__( 'How often the Auto-Import should reoccur. Allowed values: hourly, twicedaily, daily', 'spotim-comments' ),
+                'value' => $this->options->get( 'import_recurrence' )
+            )
+        );
+		*/
+
+        add_settings_field(
+            'auto_import',
+            esc_html__( 'Auto Import', 'spotim-comments' ),
+            array( 'SpotIM_Form_Helper', 'radio_fields' ),
+            $this->options->slug,
+            'import_settings_section',
+            array(
+                'id' => 'auto_import',
+                'page' => $this->options->slug,
+                'description' => esc_html__( 'Enable Auto-Import and how ofter should it reoccur.', 'spotim-comments' ),
+                'fields' => array(
+                    '0' => esc_html__( 'No', 'spotim-comments' ),
+                    'hourly' => esc_html__( 'Hourly', 'spotim-comments' ),
+                    'twicedaily' => esc_html__( 'Twice Daily', 'spotim-comments' ),
+                    'daily' => esc_html__( 'Daily', 'spotim-comments' ),
+                ),
+                'value' => $this->options->get( 'auto_import' )
+            )
+        );
+
         add_settings_field(
             'import_button',
-            '',
+            esc_html__( 'Manual Import', 'spotim-comments' ),
             array( 'SpotIM_Form_Helper', 'import_button' ),
             $this->options->slug,
             'import_settings_section',
             array(
                 'import_button' => array(
                     'id' => 'import_button',
-                    'text' => esc_html__( 'Import', 'spotim-comments' )
+                    'text' => esc_html__( 'Import Now!', 'spotim-comments' )
                 ),
                 'cancel_import_link' => array(
                     'id' => 'cancel_import_link',
@@ -201,5 +296,6 @@ class SpotIM_Settings_Fields {
                 )
             )
         );
+
     }
 }
