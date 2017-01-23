7e06a18bcf3e792cf337bf50dc46641271158479
Rami Yushuvaev <r_a_m_i@hotmail.com>
2017-01-23 10:35:49 +0200

    Add recirculation template

---
+++ templates/recirculation-template.php
@@ -0,0 +1,2 @@
+﻿<div data-spotim-module="recirculation" data-spot-id="<?php echo $spot_id; ?>"></div>
+<script type="text/javascript" src="https://recirculation.spot.im/spot/<?php echo $spot_id; ?>"></script>
