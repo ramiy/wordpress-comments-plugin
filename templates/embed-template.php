59e3556c4207661daf0596b038bcc7d06c30038a
Rami Yushuvaev <r_a_m_i@hotmail.com>
2017-02-05 13:34:19 +0200

    update templates

--- templates/embed-template.php
+++ templates/embed-template.php
@@ -1,5 +1,4 @@
 <script type="text/javascript">
-    // v3.0.0
     window.SPOTIM = {
         spotId: <?php echo wp_json_encode( $this->get( 'spot_id' ) ); ?>,
         parentElement: document.body,
