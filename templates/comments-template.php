0f8d4dc8b364d01f8aca0c368f09308b42be9d1a
Rami Yushuvaev <r_a_m_i@hotmail.com>
2017-02-09 11:48:22 +0200

    Escaping HTML attributes in comments template

--- templates/comments-template.php
+++ templates/comments-template.php
@@ -1,4 +1,4 @@
 <div class="spot-im-comments comments-area">
-    <div class="spot-im-frame-inpage" data-post-id="<?php the_ID(); ?>" data-disqus-url="<?php the_permalink(); ?>" data-disqus-identifier="<?php the_ID(); ?> <?php echo esc_url( site_url() ); ?>/?p=<?php the_ID(); ?>" data-community-question="<?php echo esc_attr( get_post_meta( get_the_id(), 'spotim_display_question', true ) ); ?>">
+    <div class="spot-im-frame-inpage" data-post-id="<?php echo esc_attr( get_the_ID() ); ?>" data-disqus-url="<?php echo esc_url( get_permalink() ); ?>" data-disqus-identifier="<?php the_ID(); ?> <?php echo esc_url( site_url() ); ?>/?p=<?php the_ID(); ?>" data-community-question="<?php echo esc_attr( get_post_meta( get_the_id(), 'spotim_display_question', true ) ); ?>">
     </div>
 </div>
