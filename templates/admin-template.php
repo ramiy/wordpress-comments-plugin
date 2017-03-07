734b3487e2ed9834de84de6bdecd840d8a7ca61a
Rami Yushuvaev <r_a_m_i@hotmail.com>
2017-03-07 22:57:26 +0200

    Use "Yoda Condition Checks" to display tabs in the admin screen

--- templates/admin-template.php
+++ templates/admin-template.php
@@ -2,10 +2,10 @@
     <h1><?php echo get_admin_page_title(); ?></h1>
 
     <nav class="nav-tab-wrapper">
-        <a href="<?php echo esc_attr( '?page=' . $this->slug . '&tab=general' ); ?>" class="nav-tab <?php echo $this->active_tab === 'general' ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'General', 'spotim-comments' ); ?></a>
-        <a href="<?php echo esc_attr( '?page=' . $this->slug . '&tab=display' ); ?>" class="nav-tab <?php echo $this->active_tab === 'display' ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'Display', 'spotim-comments' ); ?></a>
-        <a href="<?php echo esc_attr( '?page=' . $this->slug . '&tab=import'  ); ?>" class="nav-tab <?php echo $this->active_tab === 'import'  ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'Import',  'spotim-comments' ); ?></a>
-        <a href="<?php echo esc_attr( '?page=' . $this->slug . '&tab=export'  ); ?>" class="nav-tab <?php echo $this->active_tab === 'export'  ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'Export',  'spotim-comments' ); ?></a>
+        <a href="<?php echo esc_attr( '?page=' . $this->slug . '&tab=general' ); ?>" class="nav-tab <?php echo ( 'general' === $this->active_tab ) ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'General', 'spotim-comments' ); ?></a>
+        <a href="<?php echo esc_attr( '?page=' . $this->slug . '&tab=display' ); ?>" class="nav-tab <?php echo ( 'display' === $this->active_tab ) ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'Display', 'spotim-comments' ); ?></a>
+        <a href="<?php echo esc_attr( '?page=' . $this->slug . '&tab=import'  ); ?>" class="nav-tab <?php echo ( 'import'  === $this->active_tab ) ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'Import',  'spotim-comments' ); ?></a>
+        <a href="<?php echo esc_attr( '?page=' . $this->slug . '&tab=export'  ); ?>" class="nav-tab <?php echo ( 'export'  === $this->active_tab ) ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'Export',  'spotim-comments' ); ?></a>
     </nav>
 
     <form method="post" action="options.php">
