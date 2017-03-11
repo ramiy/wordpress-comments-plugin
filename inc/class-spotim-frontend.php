a8b176198b8282cc0c19fb25e5da1041cada7c0d
Rami Yushuvaev <r_a_m_i@hotmail.com>
2017-03-11 02:09:07 +0200

    Remove redundant space char

--- inc/class-spotim-frontend.php
+++ inc/class-spotim-frontend.php
@@ -77,7 +77,7 @@ class SpotIM_Frontend {
 
         // Bail if Spot.IM Comments are disabled for this this specific content item
         $specific_display = get_post_meta( absint( $post->ID ), 'spotim_display_comments', true );
-        $specific_display = in_array( $specific_display , array( 'enable', 'disable' ), true ) ? $specific_display : 'enable';
+        $specific_display = in_array( $specific_display, array( 'enable', 'disable' ), true ) ? $specific_display : 'enable';
         if ( 'disable' === $specific_display )
             return false;
 
