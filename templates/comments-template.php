c6c9d4b6279bcff874107fc1e0d22bb9de38d703
Rami Yushuvaev <r_a_m_i@hotmail.com>
2017-06-29 19:25:29 +0300

    Filter data attributes

--- templates/comments-template.php
+++ templates/comments-template.php
@@ -1,4 +1,9 @@
 <div class="spot-im-comments comments-area">
-    <div class="spot-im-frame-inpage" data-post-id="<?php echo esc_attr( get_the_ID() ); ?>" data-facebook-url="<?php echo esc_url( get_permalink() ); ?>" data-disqus-url="<?php echo esc_url( get_permalink() ); ?>" data-disqus-identifier="<?php the_ID(); ?> <?php echo esc_url( site_url() ); ?>/?p=<?php the_ID(); ?>" data-community-question="<?php echo esc_attr( get_post_meta( get_the_id(), 'spotim_display_question', true ) ); ?>">
+    <div class="spot-im-frame-inpage"
+		data-post-id="<?php echo esc_attr( apply_filters( 'spotim_comments_post_id', get_the_ID() ) ); ?>"
+		data-facebook-url="<?php echo esc_url( apply_filters( 'spotim_comments_facebook_url', get_permalink() ) ); ?>"
+		data-disqus-url="<?php echo esc_url( apply_filters( 'spotim_comments_disqus_url', get_permalink() ) ); ?>"
+		data-disqus-identifier="<?php echo apply_filters( 'spotim_comments_disqus_identifier',  ( get_the_id() . ' ' . esc_url( site_url( '/?p=' . get_the_id() ) ) ) ); ?>"
+		data-community-question="<?php echo esc_attr( apply_filters( 'spotim_comments_community_question',  get_post_meta( get_the_id(), 'spotim_display_question', true ) ) ); ?>">
     </div>
 </div>
