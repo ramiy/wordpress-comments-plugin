b3c76d0a02e075052602b35c66e95e360a004f31
Rami Yushuvaev <r_a_m_i@hotmail.com>
2017-07-11 22:41:33 +0300

    Set custom "data-disqus-identifier" attribute based on the field selected by the user

--- templates/comments-template.php
+++ templates/comments-template.php
@@ -1,13 +1,27 @@
+<?php
+$options = SpotIM_Options::get_instance();
+switch( $options->get( 'disqus_identifier' ) ) {
+    case 'id':
+        $disqus_identifier = get_the_id();
+        break;
+    case 'short_url':
+        $disqus_identifier = esc_url( site_url( '/?p=' . get_the_id() ) );
+        break;
+    case 'id_short_url':
+    default:
+        $disqus_identifier = get_the_id() . ' ' . esc_url( site_url( '/?p=' . get_the_id() ) );
+}
+?>
 <div class="spot-im-comments comments-area">
     <div class="spot-im-frame-inpage"
         data-post-id="<?php echo esc_attr( apply_filters( 'spotim_comments_post_id', get_the_ID() ) ); ?>"
         data-post-url="<?php echo esc_url( apply_filters( 'spotim_comments_post_url', get_permalink() ) ); ?>"
-        data-messages-count="<?php $options = SpotIM_Options::get_instance(); echo esc_attr( $options->get( 'comments_per_page' ) ); ?>"
+        data-messages-count="<?php echo esc_attr( apply_filters( 'spotim_comments_messages_count', $options->get( 'comments_per_page' ) ) ); ?>"
         data-wp-import-endpoint="<?php echo esc_url( apply_filters( 'spotim_comments_feed_link', get_post_comments_feed_link( get_the_id(), 'spotim' ) ) ); ?>"
         data-facebook-url="<?php echo esc_url( apply_filters( 'spotim_comments_facebook_url', get_permalink() ) ); ?>"
         data-disqus-url="<?php echo esc_url( apply_filters( 'spotim_comments_disqus_url', get_permalink() ) ); ?>"
         data-disqus-short-url="<?php echo esc_url( apply_filters( 'spotim_comments_disqus_short_url', site_url( '/?p=' . get_the_id() ) ) ); ?>"
-        data-disqus-identifier="<?php echo apply_filters( 'spotim_comments_disqus_identifier',  ( get_the_id() . ' ' . esc_url( site_url( '/?p=' . get_the_id() ) ) ) ); ?>"
+        data-disqus-identifier="<?php echo apply_filters( 'spotim_comments_disqus_identifier', $disqus_identifier ); ?>"
         data-community-question="<?php echo esc_attr( apply_filters( 'spotim_comments_community_question',  get_post_meta( get_the_id(), 'spotim_display_question', true ) ) ); ?>">
     </div>
 </div>
