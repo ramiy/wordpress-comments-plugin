621d8c8f8240ce6248c4c9219f479f18e28fef03
Rami Yushuvaev <r_a_m_i@hotmail.com>
2017-07-11 22:12:39 +0300

    Add "data-disqus-short-url" attribute with short URL

--- templates/comments-template.php
+++ templates/comments-template.php
@@ -6,6 +6,7 @@
         data-wp-import-endpoint="<?php echo esc_url( apply_filters( 'spotim_comments_feed_link', get_post_comments_feed_link( get_the_id(), 'spotim' ) ) ); ?>"
         data-facebook-url="<?php echo esc_url( apply_filters( 'spotim_comments_facebook_url', get_permalink() ) ); ?>"
         data-disqus-url="<?php echo esc_url( apply_filters( 'spotim_comments_disqus_url', get_permalink() ) ); ?>"
+        data-disqus-short-url="<?php echo esc_url( apply_filters( 'spotim_comments_disqus_short_url', esc_url( site_url( '/?p=' . get_the_id() ) ) ) ); ?>"
         data-disqus-identifier="<?php echo apply_filters( 'spotim_comments_disqus_identifier',  ( get_the_id() . ' ' . esc_url( site_url( '/?p=' . get_the_id() ) ) ) ); ?>"
         data-community-question="<?php echo esc_attr( apply_filters( 'spotim_comments_community_question',  get_post_meta( get_the_id(), 'spotim_display_question', true ) ) ); ?>">
     </div>
